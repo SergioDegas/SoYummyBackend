d17a3202c8459a6639862a289981616b1974299a
Sergey Miniaylo <po4tovuj@gmail.com>
2023-03-31 13:56:14 +0300

    remove middleware bodyValidate

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,11 +1,9 @@
-const validateBody = require('./validateBody');
 const authenticate = require('./authenticate');
 const isValidId = require('./isValidId');
 const isBodyNotEmpty = require('./isBodyNotEmpty');
 const schemaValidator = require('./schemaValidator');
 
 module.exports = {
-  validateBody,
   authenticate,
   isValidId,
   isBodyNotEmpty,
--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {
-  validateBody,
-  isBodyNotEmpty,
-  schemaValidator,
-} = require('../../middlewares');
+const { isBodyNotEmpty, schemaValidator } = require('../../middlewares');
 const { register, login } = require('../../controllers/users');
 
 router.post('/register', isBodyNotEmpty(), schemaValidator, register);
