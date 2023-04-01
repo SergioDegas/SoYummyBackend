4e179716b5451806227e99ce8020e2df01329e41
Sergey Miniaylo <po4tovuj@gmail.com>
2023-04-01 23:50:20 +0300

    api-docs to test and check

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -3,7 +3,12 @@ const router = express.Router();
 const { isBodyNotEmpty, schemaValidator, authenticate } = require('../../middlewares');
 const { register, login, current, logout } = require('../../controllers/users');
 
-router.post('/register', isBodyNotEmpty(), schemaValidator, register);
+router.post(
+  '/register',
+  isBodyNotEmpty('skdfsdlkjfsdklfsdjf'),
+  schemaValidator,
+  register
+);
 router.post('/login', isBodyNotEmpty(), schemaValidator, login);
 router.get('/current', schemaValidator, authenticate, current);
 router.post('/logout', schemaValidator, authenticate, logout);
