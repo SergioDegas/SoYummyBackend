42f8fc863c517cd9e3b4874a81b94bfd60c33348
Igor Melnyk <igor.melnyk2011@gmail.com>
2023-04-02 21:42:13 +0300

    some small fix

--- src/routes/api/ingredients.js
+++ src/routes/api/ingredients.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const ingredientsCtrl = require("../../controllers/ingredients");
-// const { authenticate } = require("../../middlewares");
+const { authenticate } = require("../../middlewares");
 
 const router = express.Router();
 
-router.get("/list", 
-// authenticate, 
-ingredientsCtrl.getIngredientsList);
+router.get("/list", authenticate, ingredientsCtrl.getIngredientsList);
 
 module.exports = router;
