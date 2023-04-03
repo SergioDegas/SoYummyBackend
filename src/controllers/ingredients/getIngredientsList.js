4d9198b80a01c12f277349bdf5c84c7886e5c068
Igor Melnyk <igor.melnyk2011@gmail.com>
2023-04-03 21:37:21 +0300

    fix re-export and name of controller

--- src/controllers/ingredients/getIngredientsList.js
+++ src/controllers/ingredients/getIngredientsList.js
@@ -1,21 +1,21 @@
 const { httpError } = require("../../helpers");
-const {Ingredient} = require("../../models");
+const { Ingredient } = require("../../models");
 
 const getIngredients = async (req, res, next) => {
-  const page = parseInt(req.query.page) || 1;
-  const limit = parseInt(req.query.limit) || 8;
-  const skipIndex = (page - 1) * limit;
+	const page = parseInt(req.query.page) || 1;
+	const limit = parseInt(req.query.limit) || 8;
+	const skipIndex = (page - 1) * limit;
 
-  const ingredientsList = await Ingredient.find().skip(skipIndex).limit(limit);
-  if (!ingredientsList) {
-    throw httpError(404, "Not found");
-  }
+	const ingredients = await Ingredient.find().skip(skipIndex).limit(limit);
+	if (!ingredients) {
+		throw httpError(404, "Not found");
+	}
 
-  res.status(200).json({
-    status: 200,
-    message: "success",
-    data: { ingredients: ingredients },
-  });
+	res.status(200).json({
+		status: 200,
+		message: "success",
+		data: { ingredients },
+	});
 };
 
-module.exports = { getIngredients };
+module.exports = getIngredients;
