8760947bef4d30444a1304c1a75750ca3c375038
Sergii Ternytskyi <serhij86@gmail.com>
2023-04-03 16:33:46 +0300

    Update getCategoryList.js

--- src/controllers/recipes/getCategoryList.js
+++ src/controllers/recipes/getCategoryList.js
@@ -4,10 +4,6 @@ const { categories } = require("../../data");
 const getCategoryList = async (req, res) => {
 	const categoryList = [...categories].sort((a, b) => a.localeCompare(b));
 
-	if (!categoryList) {
-		throw httpError(404, "Categories not found!");
-	}
-
 	res.status(200).json({
 		categories: categoryList,
 	});
