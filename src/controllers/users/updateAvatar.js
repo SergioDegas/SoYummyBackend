4a4ba4021ea4f2865e5a9e3e58066b86aa4eb6dc
Viktor Annyenkov <annyenkov@gmail.com>
2023-04-05 01:59:04 +0300

    Update updateAvatar.js

--- src/controllers/users/updateAvatar.js
+++ src/controllers/users/updateAvatar.js
@@ -1,8 +1,9 @@
 const { User } = require("../../models");
 
 const updateAvatar = async (req, res) => {
+  const { name } = req.headers;
   const id = req.user._id;
-  await User.findByIdAndUpdate(id, {avatarURL: req.file.path})
+  await User.findByIdAndUpdate(id, {name: name, avatarURL: req.file.path})
   res.status(200).json({
     description: "success",
   })
