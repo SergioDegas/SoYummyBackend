779567ee9218e591cc30a3cc553f6424b3d3d039
Sergii Ternytskyi <serhij86@gmail.com>
2023-04-04 23:05:56 +0300

    added ingredients from otherDB

--- src/controllers/recipes/getRecipesById.js
+++ src/controllers/recipes/getRecipesById.js
@@ -5,6 +5,7 @@ const getRecipeById = async (req, res) => {
 	const { id: recipeId } = req.params;
 
 	const result = await recipes.getRecipeById(recipeId);
+	console.log(result);
 
 	if (!result) {
 		throw httpError(404, "Recipe not found!");
--- src/service/recipes.js
+++ src/service/recipes.js
@@ -1,3 +1,4 @@
+const { mongoose } = require("mongoose");
 const { Recipe } = require("../models");
 
 const getRecipesByCategory = async (category, skip = 0, limit = 8) => {
@@ -8,13 +9,34 @@ const getRecipesByCategory = async (category, skip = 0, limit = 8) => {
 };
 
 const getRecipeById = async (recipeId) => {
-	return await Recipe.findOne({ _id: recipeId });
+	const ObjectId = mongoose.Types.ObjectId;
+
+	return await Recipe.aggregate([
+		{ $match: { _id: ObjectId(`${recipeId}`) } },
+		{ $lookup: { from: "ingredients", localField: "ingredients.id", foreignField: "_id", as: "ingredientsInfo" } },
+		{
+			$set: {
+				ingredients: {
+					$map: {
+						input: "$ingredients",
+						in: {
+							$mergeObjects: [
+								"$$this",
+								{ $arrayElemAt: ["$ingredientsInfo", { $indexOfArray: ["$ingredientsInfo._id", "$$this.id"] }] },
+							],
+						},
+					},
+				},
+			},
+		},
+		{ $unset: ["ingredientsInfo", "ingredients.id"] },
+	]);
 };
 
 const getRecipesBySet = async (skip, limit) => {
 	return await Recipe.aggregate([
 		{ $sort: { category: 1, updatedAt: -1 } },
-		{ $group: { _id: "$category", recipes: { $push: { title: "$title", thumb: "$thumb" } } } },
+		{ $group: { _id: "$category", recipes: { $push: { title: "$title", thumb: "$thumb", _id: "$_id" } } } },
 		{ $sort: { _id: 1 } },
 		{ $skip: skip },
 		{ $limit: limit },
