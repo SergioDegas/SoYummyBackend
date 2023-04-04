601668e8af1712cfc104c88bb95dabc2c3c6acc8
Viktor Annyenkov <annyenkov@gmail.com>
2023-04-04 15:04:13 +0300

    Update users.js

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { isBodyNotEmpty, schemaValidator, authenticate } = require("../../middlewares");
-const { getShoppingList, updateShoppingList, current } = require("../../controllers/users");
+const { isBodyNotEmpty, schemaValidator, authenticate, uploadCloud } = require("../../middlewares");
+const { getShoppingList, updateShoppingList, current, updateAvatar } = require("../../controllers/users");
 
 router.get("/shopping-list", authenticate, getShoppingList);
 router.patch("/shopping-list", isBodyNotEmpty(), authenticate, schemaValidator, updateShoppingList);
 router.get("/current", schemaValidator, authenticate, current);
-router.post("/")
+router.post('/updateAvatar', authenticate, uploadCloud.single("image"), updateAvatar);
 
 module.exports = router;
