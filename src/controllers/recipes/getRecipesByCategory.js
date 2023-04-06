3bcbbfdb9919fa63f3e8f77428d77c301efe32f1
Sergii Ternytskyi <serhij86@gmail.com>
2023-04-06 11:12:53 +0300

    Update getRecipesByCategory.js

--- src/controllers/recipes/getRecipesByCategory.js
+++ src/controllers/recipes/getRecipesByCategory.js
@@ -1,15 +1,15 @@
 const { httpError } = require("../../helpers");
 const { recipes } = require("../../service");
-const { categories } = require("../../helpers");
+const { categories } = require("../../data");
 const { skipPageHandler, limitHandler } = require("../../helpers");
 
 const getRecipesByCategory = async (req, res) => {
 	const { category } = req.params;
 	const { page = 1, limit = 8 } = req.query;
 
-	// if (!categories.includes(category)) {
-	// 	throw httpError(400, "Bad request");
-	// }
+	if (!categories.includes(category)) {
+		throw httpError(400, "Bad request");
+	}
 
 	const result = await recipes.getRecipesByCategory(category, skipPageHandler(page, limit), limitHandler(limit));
 	const totalResult = await recipes.getRecipesByCategory(category, 0, 0);
