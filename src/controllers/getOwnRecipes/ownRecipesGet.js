13a50e9013a1cd2f29a4d06dec36b8d6b2082cde
Serhii Lazarenok <serhiilazar90@gmail.com>
2023-04-09 02:25:23 +0300

    fix get own-recipes

--- src/controllers/getOwnRecipes/ownRecipesGet.js
+++ src/controllers/getOwnRecipes/ownRecipesGet.js
@@ -1,19 +1,18 @@
 const { httpError } = require("../../helpers");
-const {ownRecipes} = require("../../service");
-
+const { ownRecipes } = require("../../service");
 
 const ownRecipesGet = async (req, res) => {
-  if (!req.user) {
-    throw httpError(401, "Access token is missing or invalid");
-  }
+	if (!req.user) {
+		throw httpError(401, "Access token is missing or invalid");
+	}
 
-  const result = await ownRecipes.recipesServiceGet({ user: req.user, query: req.query });
-
-  res.json({
-    status: 200,
-    message: "success",
-    data: { result }
-  });
+	const result = await ownRecipes.recipesServiceGet({ user: req.user, query: req.query });
+	
+	res.json({
+		status: 200,
+		message: "success",
+		data: { result },
+	});
 };
 
 module.exports = { ownRecipesGet };
