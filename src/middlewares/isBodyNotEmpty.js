04972ddf6b3d3ef2bd8f9f34ac6739db6bf9ce02
Sergii Ternytskyi <serhij86@gmail.com>
2023-04-09 00:13:09 +0300

    Update isBodyNotEmpty.js

--- src/middlewares/isBodyNotEmpty.js
+++ src/middlewares/isBodyNotEmpty.js
@@ -1,4 +1,4 @@
-const { isEmpty } = require("lodash.isempty");
+const isEmpty = require("lodash.isempty");
 const isBodyNotEmpty = (message = "Missing required fields") => {
 	const func = (req, res, next) => {
 		if (isEmpty(req.body)) {
