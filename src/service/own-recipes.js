39bd109fd3ffc749ce68e48a15542eceda5a3656
Sergii Ternytskyi <serhij86@gmail.com>
2023-04-13 01:18:50 +0300

    totalItem added

--- src/service/own-recipes.js
+++ src/service/own-recipes.js
@@ -4,11 +4,15 @@ const recipesServiceGet = async ({ user, query }) => {
 	const { _id: owner } = user;
 	const { page = 1, limit = 8 } = query;
 	const skip = (page - 1) * limit;
-	return await Recipe.find({ owner })
+
+	const result = await Recipe.find({ owner })
 		.skip(skip)
 		.limit(limit)
 		.sort({ updatedAt: -1 })
 		.populate('owner', 'email subscription');
+	const totalItems = await Recipe.find({ owner }).countDocuments();
+
+	return { result, totalItems };
 };
 
 const createRecipe = async ({ body, owner, thumb }) => {
