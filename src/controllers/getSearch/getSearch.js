a225145e8483e86cc5c51d8e64d20d99cac9fef3
Serhii Lazarenok <serhiilazar90@gmail.com>
2023-04-16 15:42:53 +0300

    fix search service, add totalPage

--- src/controllers/getSearch/getSearch.js
+++ src/controllers/getSearch/getSearch.js
@@ -1,22 +1,24 @@
 const { search } = require('../../service');
 
 const getSearch = async (req, res) => {
-	const { searchTerm, page, limit, searchBy } = req.query;
-	const recipes = await search.searchService(searchTerm, page, limit, searchBy);
+  const { searchTerm, page, limit, searchBy } = req.query;
+  const {recipes, totalPage} = await search.searchService(searchTerm, page, limit, searchBy, true);
 
-	if (!recipes || recipes.length === 0) {
-		return res.json({
-			status: 200,
-			message: 'success',
-			recipes: { recipes: [] },
-		});
-	}
+  if (!recipes || recipes.length === 0) {
+    return res.json({
+      status: 200,
+      message: 'success',
+      recipes: [], 
+    });
+  }
 
-	res.json({
-		status: 200,
-		message: 'success',
-		recipes,
-	});
+  res.json({
+    status: 200,
+    message: 'success',
+    recipes,
+	totalPage
+  });
+
 };
 
 module.exports = { getSearch };
--- src/service/search.js
+++ src/service/search.js
@@ -1,81 +1,85 @@
 const { Recipe } = require('../models');
 
-const searchService = async (searchTerm, page = 1, limit = 8, searchBy) => {
+const searchService = async (searchTerm, page, limit, searchBy) => {
+    const regex = new RegExp(searchTerm, 'i');
 	const skip = (parseInt(page) - 1) * parseInt(limit);
-	const regex = new RegExp(searchTerm, 'i');
 
-	const matchQueries = {
-		title: { title: regex },
-		ingredient: { 'ingredients.name': regex },
-		family: { 'ingredients.family': regex },
-		default: {
-			$or: [{ title: regex }, { 'ingredients.name': regex }, { 'ingredients.family': regex }],
-		},
-	};
+    const matchQueries = {
+        title: { title: regex },
+        ingredient: { 'ingredients.name': regex },
+        family: { 'ingredients.family': regex },
+        default: {
+            $or: [{ title: regex }, { 'ingredients.name': regex }, { 'ingredients.family': regex }],
+        },
+    };
 
-	const matchQuery = matchQueries[searchBy] || matchQueries.default;
+    const matchQuery = matchQueries[searchBy] || matchQueries.default;
 
-	return await Recipe.aggregate([
-		{
-			$lookup: {
-				from: 'ingredients',
-				localField: 'ingredients.id',
-				foreignField: '_id',
-				as: 'ingredients',
-			},
-		},
-		{
-			$match: matchQuery,
-		},
-		{
-			$project: {
-				title: 1,
-				category: 1,
-				area: 1,
-				instructions: 1,
-				description: 1,
-				thumb: 1,
-				preview: 1,
-				time: 1,
-				popularity: 1,
-				youtube: 1,
-				ingredients: {
-					$filter: {
-						input: '$ingredients',
-						as: 'ingredient',
-						cond: {
-							$or: [
-								{
-									$regexMatch: {
-										input: '$$ingredient.name',
-										regex: regex,
-									},
-								},
-								{
-									$regexMatch: {
-										input: '$$ingredient.family',
-										regex: regex,
-									},
-								},
-								{
-									$regexMatch: {
-										input: '$title',
-										regex: regex,
-									},
-								},
-							],
-						},
-					},
-				},
-			},
-		},
-		{
-			$skip: skip,
-		},
-		{
-			$limit: parseInt(limit),
-		},
-	]);
+    const recipes = await Recipe.aggregate([
+        {
+            $lookup: {
+                from: 'ingredients',
+                localField: 'ingredients.id',
+                foreignField: '_id',
+                as: 'ingredients',
+            },
+        },
+        {
+            $match: matchQuery,
+        },
+        {
+            $project: {
+                title: 1,
+                category: 1,
+                area: 1,
+                instructions: 1,
+                description: 1,
+                thumb: 1,
+                preview: 1,
+                time: 1,
+                popularity: 1,
+                youtube: 1,
+                ingredients: {
+                    $filter: {
+                        input: '$ingredients',
+                        as: 'ingredient',
+                        cond: {
+                            $or: [
+                                {
+                                    $regexMatch: {
+                                        input: '$$ingredient.name',
+                                        regex: regex,
+                                    },
+                                },
+                                {
+                                    $regexMatch: {
+                                        input: '$$ingredient.family',
+                                        regex: regex,
+                                    },
+                                },
+                                {
+                                    $regexMatch: {
+                                        input: '$title',
+                                        regex: regex,
+                                    },
+                                },
+                            ],
+                        },
+                    },
+                },
+            },
+        },
+        {
+            $facet: {
+              recipes: [{ $skip: parseInt(skip) }, { $limit: parseInt(limit) }],
+              count: [{ $count: 'total' }],
+            },
+        },
+    ]);
+
+    const totalRecipes = recipes[0].count[0].total;
+    const totalPage = Math.ceil(totalRecipes / parseInt(limit));
+    return { recipes: recipes[0].recipes, totalRecipes, totalPage };
 };
 
 module.exports = { searchService };
